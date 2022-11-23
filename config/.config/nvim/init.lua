0b39ed9eca457283049a04560e76490bd450cbdc
Will <johnsteele2009@live.co.uk>
2022-11-23 21:54:39 +0000

    first commit

---
+++ config/.config/nvim/init.lua
@@ -0,0 +1,80 @@
+local o = vim.opt
+o.scrolloff = 10
+o.backup = false
+o.cursorline = true
+o.number = true
+o.relativenumber = true
+o.wrap = false
+o.expandtab = true
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.autoindent = true
+o.smartindent = true
+o.swapfile = false
+o.incsearch = true
+o.hlsearch = false
+o.showmatch = true
+o.termguicolors = true
+o.syntax = 'on'
+
+vim.g.mapleader = ' '
+local map = vim.keymap.set
+map('n', '<leader>h', '<cmd>wincmd h<cr>')
+map('n', '<leader>j', '<cmd>wincmd j<cr>')
+map('n', '<leader>k', '<cmd>wincmd k<cr>')
+map('n', '<leader>l', '<cmd>wincmd l<cr>')
+map('n', '<leader>ff', '<cmd>Files<cr>')
+map('n', '<leader>fg', '<cmd>GFiles<cr>')
+map('n', '<leader>fa', '<cmd>Ag<cr>')
+map('n', 'B', '^')
+map('n', 'E', '$')
+map('n', '$', '<nop>')
+map('n', '^', '<nop>')
+map('v', 'f', '!black -q -l 30 --fast - <cr>')
+
+require('colourscheme')
+require('statusline')
+require('treesitter')
+require('_lspconfig')
+require('_telescope')
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'tanvirtin/monokai.nvim'
+  use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use 'neovim/nvim-lspconfig'
+  use {
+      'nvim-treesitter/nvim-treesitter',
+      run = ':TSUpdate'
+  }
+  use "nvim-lua/plenary.nvim"
+  use {
+      'nvim-telescope/telescope.nvim',
+      tag = '0.1.0'
+  }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
+
---
+++ vim/.vimrc
@@ -0,0 +1,63 @@
+syntax on
+set cursorline
+set number
+set noerrorbells
+set expandtab
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+set scrolloff=10
+set nu
+set relativenumber
+set nowrap
+set autoindent
+set smartindent
+set noswapfile
+set nobackup
+set undodir=~/.vim/undodir
+set undofile
+set incsearch
+set hlsearch
+set showmatch
+set colorcolumn=80
+set background=dark
+set tags=tags;/
+highlight ColorColumn ctermbg=0 guibg=lightgrey
+
+let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+if empty(glob(data_dir . '/autoload/plug.vim'))
+  silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+call plug#begin()
+Plug 'junegunn/fzf', {'do': { -> fzf#install() } }
+Plug 'junegunn/fzf.vim'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'morhetz/gruvbox'
+Plug 'crusoexia/vim-monokai'
+call plug#end()
+
+"" autocmd vimenter * ++nested colorscheme gruvbox
+"" let g:gruvbox_contrast_dark = 'medium'
+
+colorscheme monokai
+
+autocmd BufWritePre *.py :%s/\s\+$//e
+let mapleader = ' '
+
+nnoremap <leader>h :wincmd h<CR>
+nnoremap <leader>j :wincmd j<CR>
+nnoremap <leader>k :wincmd k<CR>
+nnoremap <leader>l :wincmd l<CR>
+nnoremap <leader>ff :Files<CR>
+nnoremap <leader>fg :GFiles<CR>
+nnoremap <leader>fa :Ag<CR>
+nnoremap B ^
+nnoremap E $
+nnoremap $ <nop>
+nnoremap ^ <nop>
+
+""python specific config
+
+vnoremap <leader>f !black -q -l 30 --fast -<CR>
