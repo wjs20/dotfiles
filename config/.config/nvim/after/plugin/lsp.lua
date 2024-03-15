cc6ba2dbc33f138b96bcf1ae6d04a307a0d8f1a7
William Steele <johnsteele2009@live.co.uk>
2024-03-15 10:13:49 +0000

    Added minimum word length to completions other than snippets

--- config/.config/nvim/after/plugin/lsp.lua
+++ config/.config/nvim/after/plugin/lsp.lua
@@ -89,13 +89,13 @@ cmp.setup({
 
     }),
     sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
+        { name = 'nvim_lsp', keyword_length = 5},
         -- { name = 'vsnip' }, -- For vsnip users.
         { name = 'luasnip' }, -- For luasnip users.
         -- { name = 'ultisnips' }, -- For ultisnips users.
         -- { name = 'snippy' }, -- For snippy users.
     }, {
-        { name = 'buffer' },
+        { name = 'buffer', keyword_length = 5},
     }),
 })
 
