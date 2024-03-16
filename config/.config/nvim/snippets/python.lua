285b730752f1561cce4b55ab92486fc80bc719a4
William Steele <johnsteele2009@live.co.uk>
2024-03-16 17:59:23 +0000

    Added better python class snippet

--- config/.config/nvim/snippets/python.lua
+++ config/.config/nvim/snippets/python.lua
@@ -1,3 +1,17 @@
+
+function collect_instance_attrs(argnode_text, parent)
+    local instance_attrs = argnode_text[1][1]
+    if not string.find(instance_attrs, ",") then
+        return "self." .. instance_attrs .. " = " .. instance_attrs
+    end
+    local attributes = {}
+    for value in string.gmatch(instance_attrs, "([^,]+)") do
+        value = string.gsub(value, "^%s*(.-)%s*$", "%1") -- Trim whitespace
+        table.insert(attributes, 'self.' .. value .. ' = ' .. value)
+    end
+    return attributes
+end
+
 return {
     s("tcview", fmt([[
     class {}Test(TestCase):
@@ -29,6 +43,17 @@ return {
     ]], {
         i(1),
         i(0)
-    }))
+    })),
+    s("myclass", fmt([[
+    class {}:
+        def __init__(self, {}):
+            {}
 
+        {}
+    ]], {
+       i(1, "ClassName"),
+       i(2, "args", {key = "args-key"}),
+       isn(nil, { f(collect_instance_attrs, k("args-key") ) }, "$PARENT_INDENT\t\t"),
+       i(0),
+    }))
 }
