0c5ad72f77ce3af2d1044cc98286a7f8022f1442
William Steele <johnsteele2009@live.co.uk>
2024-07-20 13:00:02 +0100

    Added snippet for blog

---
+++ config/.config/nvim/snippets/markdown.lua
@@ -0,0 +1,15 @@
+return {
+    s("front-matter", fmt([[
+        ---
+        layout: post
+        title:  "{}"
+        date:   2024-01-02 22:41:33 +0000
+        categories: {}
+        ---
+        {}
+    ]], {
+       i(1),
+       i(2),
+       i(0),
+    })),
+}
