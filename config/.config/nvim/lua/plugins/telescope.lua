b8006aeb1625792c0f20906cb50a08235a7cec57
William Steele <you@example.com>
2025-05-21 09:44:41 +0100

    Added new Telescope keymaps

--- config/.config/nvim/lua/plugins/telescope.lua
+++ config/.config/nvim/lua/plugins/telescope.lua
@@ -4,12 +4,13 @@ return {
         tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' },
         keys = {
-            { '<leader>ff', '<cmd>Telescope find_files<cr>',            desc = '[F]ind [F]iles' },
-            { '<leader>fg', '<cmd>Telescope live_grep<cr>',             desc = '[L]ive [G]rep' },
-            { '<leader>fb', '<cmd>Telescope buffers sort_mru=true<cr>', desc = '[F]ind [B]uffers' },
-            { '<leader>fd', '<cmd>Telescope diagnostics<cr>',           desc = '[F]ind [D]iagnostics' },
-            { '<leader>fs', '<cmd>Telescope lsp_document_symbols<cr>',  desc = '[F]ind [D]ocument [S]ymbols' },
-            { '<leader>fk', '<cmd>Telescope keymaps<cr>',               desc = '[K]ey[M]aps' }
+            { '<leader>ff', '<cmd>Telescope find_files<cr>',                                     desc = '[F]ind [F]iles' },
+            { '<leader>fg', '<cmd>Telescope live_grep<cr>',                                      desc = '[L]ive [G]rep' },
+            { '<leader>fb', '<cmd>Telescope git_branches<cr>',                                   desc = '[L]ive [G]rep' },
+            { '<leader>b',  '<cmd>Telescope buffers sort_mru=true<cr>',                          desc = '[F]ind [B]uffers' },
+            { '<leader>fd', '<cmd>Telescope diagnostics<cr>',                                    desc = '[F]ind [D]iagnostics' },
+            { '<leader>fs', '<cmd>Telescope lsp_document_symbols ignore_symbols="variable"<cr>', desc = '[F]ind [D]ocument [S]ymbols' },
+            { '<leader>fk', '<cmd>Telescope keymaps<cr>',                                        desc = '[K]ey[M]aps' }
         }
     }
 }
