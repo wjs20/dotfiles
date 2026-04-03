a6b24c234b1aa39d72b64e965fa6360e890c2cdc
William Steele <you@example.com>
2026-04-03 15:44:39 +0100

    Added treesitter incremental selection

--- config/dot-config/nvim/lua/plugins/treesitter.lua
+++ config/dot-config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,25 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = function()
             require("nvim-treesitter.install").update({ with_sync = true })()
+        end,
+        lazy = false,
+        config = function()
+            require'nvim-treesitter.configs'.setup {
+                -- Modules and its options go here
+                ensure_installed = { 'python', 'lua' },
+                highlight = { enable = true },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<C-i>",
+                        node_incremental = "<C-i>",
+                        scope_incremental = "grc",
+                        node_decremental = "<C-d>",
+                    }
+                },
+                textobjects = { enable = true },
+            }
         end
     }
+
 }
