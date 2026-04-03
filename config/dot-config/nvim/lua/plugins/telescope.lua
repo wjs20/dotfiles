cbb9be3af5d63eb3d2ad0ee8ef2bf5245818c210
William Steele <you@example.com>
2026-04-03 15:39:46 +0100

    Cleaned up Telescope config

--- config/dot-config/nvim/lua/plugins/telescope.lua
+++ config/dot-config/nvim/lua/plugins/telescope.lua
@@ -2,17 +2,16 @@ return {
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.8',
+        lazy = false,
         dependencies = { 'nvim-lua/plenary.nvim' },
         keys = {
             { '<leader>ff', '<cmd>Telescope find_files<cr>',                                     desc = '[F]ind [F]iles' },
-            { '<leader>fg', '<cmd>Telescope live_grep<cr>',                                      desc = '[L]ive [G]rep' },
-            { '<leader>fb', '<cmd>Telescope git_branches<cr>',                                   desc = '[L]ive [G]rep' },
-            { '<leader>b',  '<cmd>Telescope buffers sort_mru=true<cr>',                          desc = '[F]ind [B]uffers' },
+            { '<leader>fg', '<cmd>Telescope live_grep<cr>',                                      desc = '[F]ind [G]rep' },
+            { '<leader>fb', '<cmd>Telescope buffers sort_mru=true<cr>',                          desc = '[F]ind [B]uffers' },
             { '<leader>fd', '<cmd>Telescope diagnostics<cr>',                                    desc = '[F]ind [D]iagnostics' },
-            { '<leader>fs', '<cmd>Telescope lsp_document_symbols ignore_symbols="variable"<cr>', desc = '[F]ind [D]ocument symbols' },
-            { '<leader>fw', '<cmd>Telescope lsp_workspace_symbols query=""<cr>',                 desc = '[F]ind [W]orkspace symbols' },
-            { '<leader>fk', '<cmd>Telescope keymaps<cr>',                                        desc = '[K]ey[M]aps' },
-            { '<leader>ft', '<cmd>Telescope tagstack<cr>',                                       desc = '[K]ey[M]aps' }
+            { '<leader>fs', '<cmd>Telescope lsp_document_symbols ignore_symbols="variable"<cr>', desc = '[F]ind document [S]ymbols' },
+            { '<leader>fk', '<cmd>Telescope keymaps<cr>',                                        desc = '[F]ind[K]eymaps' },
+            { '<leader>ft', '<cmd>Telescope tagstack<cr>',                                       desc = '[T]ey[M]aps' }
         }
     }
 }
